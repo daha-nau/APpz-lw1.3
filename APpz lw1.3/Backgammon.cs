91304ff7b1376fa712e1e52f79662f00bb1c6082
Daha <daha.kyiv@gmail.com>
2020-04-24 00:05:17 +0300

    classes Backgammon and Chess updated

--- APpz lw1.3/Backgammon.cs
+++ APpz lw1.3/Backgammon.cs
@@ -6,7 +6,21 @@ using System.Threading.Tasks;
 
 namespace APpz_lw1._3
 {
-    public class Backgammon 
+    public class Backgammon : Game
     {
+        private int[] sum;
+        public Backgammon(int players, string fieldType = "chessboard") : base(players, fieldType)
+        {
+            sum = new int[2];
+            sum[0] = 0;
+            sum[1] = 0;
+        }
+        public override int makeMove(int player)
+        {
+            int result = ((Cub)attributes[0]).getSide() + ((Cub)attributes[1]).getSide();
+            sum[player] += result;
+            return result;
+        }
+        public int getSum(int player) => sum[player];
     }
 }
--- APpz lw1.3/Chess.cs
+++ APpz lw1.3/Chess.cs
@@ -6,7 +6,15 @@ using System.Threading.Tasks;
 
 namespace APpz_lw1._3
 {
-    public class Chess 
+    public class Chess : Game
     {
+        public Chess(int players, string fieldType = "chessboard") : base(players, fieldType)
+        {
+        }
+        public override int makeMove(int player)
+        {
+            Random random = new Random();
+            return random.Next(0, 15);
+        }
     }
 }
