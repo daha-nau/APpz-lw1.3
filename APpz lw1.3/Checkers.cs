04d0ce0e0f8f57bfc42b00e9271db2e35ef942a1
Daha <daha.kyiv@gmail.com>
2020-04-23 23:31:13 +0300

    class Checkers updated.

--- APpz lw1.3/Checkers.cs
+++ APpz lw1.3/Checkers.cs
@@ -6,7 +6,16 @@ using System.Threading.Tasks;
 
 namespace APpz_lw1._3
 {
-    public class Checkers 
+    public class Checkers : Game
     {
+        public Checkers(int players, string fieldtype = "chessboard") : base(players, fieldtype)
+        {
+        }
+
+        public override int makeMove(int player)
+        {
+            Random random = new Random();
+            return ((Checker)attributes[random.Next(0, 15)]).Identifier;
+        }
     }
 }
