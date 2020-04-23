17e86fc34273320bbe586ba082e36f13f893f76f
Daha <daha.kyiv@gmail.com>
2020-04-24 00:00:03 +0300

    class Monopoly updated

--- APpz lw1.3/Monopoly.cs
+++ APpz lw1.3/Monopoly.cs
@@ -6,8 +6,22 @@ using System.Threading.Tasks;
 
 namespace APpz_lw1._3
 {
-    class Monopoly 
+    class Monopoly : Game
     {
-        
+        private int[] position;
+        public Monopoly(int players, string fieldType = "monopoly board") : base(players, fieldType)
+        {
+            position = new int[players];
+            for(int i = 0; i < players; i++)
+            {
+                position[i] = 0;
+            }
+        }
+        public override int makeMove(int player)
+        {
+            position[player] += ((Cub)attributes[0]).getSide() + ((Cub)attributes[1]).getSide();
+            position[player] = position[player] % 30;
+            return position[player];
+        }
     }
 }
